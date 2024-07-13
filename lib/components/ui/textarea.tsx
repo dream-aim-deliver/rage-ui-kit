b43cad2c5b26e1c04d9eaadfa2e77f52761aebce
Carla Bordo <carlapbg@gmail.com>
2024-07-13 20:39:01 +0200

    Temp

--- lib/components/ui/textarea.tsx
+++ lib/components/ui/textarea.tsx
@@ -6,14 +6,15 @@ export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, placeholder = "Type your message", ...props }, ref) => {
     return (
       <textarea
         className={cn(
-          "flex min-h-[40px] w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300",
+          "flex h-10 w-full rounded-lg border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 ",
           className
         )}
         ref={ref}
+        placeholder={placeholder}
         {...props}
       />
     )
