65039426f860e3c095880a6dec0f3567fe5d5355
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-07-15 21:53:07 +0200

    SourceDataTable: Fixed linting, added selection option
    - todo: test how selected items are handled
      + E.g., one page will need batch download; another one will need the selection to create a conversation
    - todo: improve actions
      + Download
      + Copy relative path
      + Copy name
    - Todo: code, apart from this, a simple upload button that will be combined with the table for one of the pages

--- lib/components/table/BaseDataTable.tsx
+++ lib/components/table/BaseDataTable.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import {
   ColumnDef,
@@ -11,9 +11,7 @@ import {
   ColumnFiltersState,
   getFilteredRowModel,
   FilterFn,
-  SortingFn,
-  sortingFns,
-} from "@tanstack/react-table"
+} from "@tanstack/react-table";
 
 import {
   Table as ShadcnTable,
@@ -22,83 +20,59 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-
-import { DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger, DropdownMenu as ShadcnDropdownMenu } from '@/ui/dropdown-menu';
-import { Button as ShadcnButton } from '@/ui/button';
+} from "@/components/ui/table";
 
 import {
-  Button,
-} from "@/components/button/Button"
-import { cn } from "@/utils/utils"
-import { ArrowBigLeft, ArrowBigRight, ChevronFirst, ChevronLast, } from "lucide-react"
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenu as ShadcnDropdownMenu,
+} from "@/ui/dropdown-menu";
+import { Button as ShadcnButton } from "@/ui/button";
+
+import { Button } from "@/components/button/Button";
+import { cn } from "@/utils/utils";
+import {
+  ArrowBigLeft,
+  ArrowBigRight,
+  ChevronFirst,
+  ChevronLast,
+} from "lucide-react";
 import { useState } from "react";
 import React from "react";
 import { Input } from "@/components/ui/input";
-import { compareItems, RankingInfo, rankItem } from "@tanstack/match-sorter-utils";
+import { RankingInfo } from "@tanstack/match-sorter-utils";
 
-
-declare module '@tanstack/react-table' {
+declare module "@tanstack/react-table" {
   //add fuzzy filter to the filterFns
   interface FilterFns {
-    fuzzy: FilterFn<unknown>
+    fuzzy: FilterFn<unknown>;
   }
   interface FilterMeta {
-    itemRank: RankingInfo
+    itemRank: RankingInfo;
   }
 }
-
-// Custom fuzzy filter function that will apply ranking info to rows (using match-sorter utils)
-export const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-
-  // 1. Rank the item
-  const itemRank = rankItem(row.getValue(columnId), value)
-
-  // 2. Store the itemRank info
-  addMeta({
-    itemRank,
-  })
-
-  // 3. Return if the item should be filtered in/out
-  return itemRank.passed
-}
-
-// Custom fuzzy sort function that will sort by rank if the row has ranking information
-export const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
-  let dir = 0
-
-  // 1. Only sort by rank if the column has ranking information
-  if (rowA.columnFiltersMeta[columnId]) {
-    dir = compareItems(
-      rowA.columnFiltersMeta[columnId]?.itemRank!,
-      rowB.columnFiltersMeta[columnId]?.itemRank!
-    )
-  }
-
-  // 2. Provide an alphanumeric fallback for when the item ranks are equal
-  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir
-}
-
 
 export interface BaseDataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
-  filterable?: boolean
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+  fuzzyFilterFunction: FilterFn<TData>;
 }
 
 export function BaseDataTable<TData, TValue>({
   columns,
   data,
-  filterable = false,
+  fuzzyFilterFunction,
   ...props
 }: BaseDataTableProps<TData, TValue>) {
-
-  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
-    []
-  )
-  const [globalFilter, setGlobalFilter] = React.useState('')
-  const [rowSelection, setRowSelection] = React.useState({})
+    [],
+  );
+  const [globalFilter, setGlobalFilter] = React.useState("");
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const fuzzyFilter = fuzzyFilterFunction;
 
   const table = useReactTable({
     data,
@@ -106,7 +80,7 @@ export function BaseDataTable<TData, TValue>({
     filterFns: {
       fuzzy: fuzzyFilter,
     },
-    globalFilterFn: 'fuzzy',
+    globalFilterFn: "fuzzy",
     onColumnFiltersChange: setColumnFilters,
     onGlobalFilterChange: setGlobalFilter,
     getCoreRowModel: getCoreRowModel(),
@@ -119,9 +93,9 @@ export function BaseDataTable<TData, TValue>({
       sorting,
       columnFilters,
       globalFilter,
-      rowSelection
-    }
-  })
+      rowSelection,
+    },
+  });
 
   const [pageSize, setPageSize] = useState(10);
   const pageSizes = [10, 25, 50, 100];
@@ -129,21 +103,18 @@ export function BaseDataTable<TData, TValue>({
   const nextPageIndex = safePageIndex + 1;
 
   return (
-
     <div className={cn("flex flex-col gap-y-4")}>
-
       <div>
         <Input
           placeholder="Search all columns..."
-          value={globalFilter ?? ''}
-          onChange={event => setGlobalFilter(event.target.value)}
+          value={globalFilter ?? ""}
+          onChange={(event) => setGlobalFilter(event.target.value)}
           className={cn("p-2 font-lg shadow border border-block")}
         />
       </div>
 
       <div className={cn("rounded-md border")}>
         <ShadcnTable {...props}>
-
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
@@ -153,11 +124,11 @@ export function BaseDataTable<TData, TValue>({
                       {header.isPlaceholder
                         ? null
                         : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
                     </TableHead>
-                  )
+                  );
                 })}
               </TableRow>
             ))}
@@ -172,40 +143,61 @@ export function BaseDataTable<TData, TValue>({
                 >
                   {row.getVisibleCells().map((cell) => (
                     <TableCell key={cell.id}>
-                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext(),
+                      )}
                     </TableCell>
                   ))}
                 </TableRow>
               ))
             ) : (
               <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
                   No results.
                 </TableCell>
               </TableRow>
             )}
           </TableBody>
-
         </ShadcnTable>
       </div>
 
-      <div className={cn("flex flex-col md:flex-row justify-between items-center gap-y-2 md:space-y-0 md:space-x-4")}>
-
-        <div className={cn("flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2")}>
+      <div
+        className={cn(
+          "flex flex-col md:flex-row justify-between items-center gap-y-2 md:space-y-0 md:space-x-4",
+        )}
+      >
+        <div
+          className={cn(
+            "flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2",
+          )}
+        >
           <ShadcnDropdownMenu>
             <span className={cn("whitespace-nowrap")}>
-              Showing&nbsp;<DropdownMenuTrigger asChild>
+              Showing&nbsp;
+              <DropdownMenuTrigger asChild>
                 <ShadcnButton
                   variant="outline"
                   className={cn("min-w-min px-2 py-1 dark:text-slate-50")}
-                >{pageSize}</ShadcnButton>
-              </DropdownMenuTrigger>&nbsp;rows
+                >
+                  {pageSize}
+                </ShadcnButton>
+              </DropdownMenuTrigger>
+              &nbsp;rows
             </span>
-            <DropdownMenuContent align="end" >
+            <DropdownMenuContent align="end">
               {pageSizes.map((size) => (
                 <DropdownMenuItem
-                  onClick={() => { table.setPageSize(size); setPageSize(size) }}
-                >{size}</DropdownMenuItem>
+                  onClick={() => {
+                    table.setPageSize(size);
+                    setPageSize(size);
+                  }}
+                >
+                  {size}
+                </DropdownMenuItem>
               ))}
             </DropdownMenuContent>
           </ShadcnDropdownMenu>
@@ -250,5 +242,5 @@ export function BaseDataTable<TData, TValue>({
         </div>
       </div>
     </div>
-  )
+  );
 }
