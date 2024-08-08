9a1cc642bfdf9868986f5042e7089862e3f0fffa
Carla Bordo <carlapbg@gmail.com>
2024-08-08 22:25:59 +0200

    Chat Page and components.- To do: test

--- lib/tailwind/config.ts
+++ lib/tailwind/config.ts
@@ -22,10 +22,20 @@ const theme = {
         from: { height: "var(--radix-accordion-content-height)" },
         to: { height: "0" },
       },
+      "slide-down": {
+        "0%": { transform: "translateY(-100%)", opacity: "0" },
+        "100%": { transform: "translateY(0)", opacity: "1" },
+      },
+      opacity: {
+        "0%": { opacity: "0" },
+        "100%": { opacity: "1" },
+      },
     },
     animation: {
       "accordion-down": "accordion-down 0.2s ease-out",
       "accordion-up": "accordion-up 0.2s ease-out",
+      "slide-down": "slide-down 0.5s ease-out",
+      opacity: "opacity 2s ease-in-out",
     },
     colors: {
       black: "#000",
