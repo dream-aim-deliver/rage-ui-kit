fc54d41e0959ef353b8433d8c00d215e72e6c0b9
maany <imptodefeat@gmail.com>
2024-08-12 13:57:04 +0200

    add source data table

--- lib/components/table/index.tsx
+++ lib/components/table/index.tsx
@@ -1,10 +1,7 @@
 export { BaseDataTable, type BaseDataTableProps } from "./BaseDataTable";
 export { SourceDataTable, type SourceDataTableProps } from "./SourceDataTable";
 export { BaseAGGrid, type BaseAGGridProps } from "./BaseAGGrid";
-export {
-  SourceDataAGGrid,
-  type SourceDataAGGridProps,
-} from "./SourceDataAGGrid";
+export { SourceDataAGGrid, type SourceDataAGGridProps } from "./SourceDataDemo";
 export {
   ConversationAGGrid,
   type ConversationAGGridProps,
---
+++ stories/components/FuckThisShit.stories.tsx
@@ -0,0 +1,82 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { SourceDataAGGrid } from "@/components/table/SourceDataDemo";
+
+const meta: Meta = {
+  title: "Components/AGGrid/FuckThisShit",
+  component: SourceDataAGGrid,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof SourceDataAGGrid>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+function randomChoice<T>(list: T[]): T {
+  const randomIndex = Math.floor(Math.random() * list.length);
+  return list[randomIndex];
+}
+
+const thousand = Array.from({ length: 1000 }, (_, i) => i + 1);
+
+function getRandomDate(start: Date, end: Date): Date {
+  const startTimestamp = start.getTime();
+  const endTimestamp = end.getTime();
+  const randomTimestamp =
+    startTimestamp + Math.random() * (endTimestamp - startTimestamp);
+  return new Date(randomTimestamp);
+}
+
+const dataGenerator = () => {
+  const topics = [
+    "wildfires",
+    "earthquakes",
+    "floods",
+    "hurricanes",
+    "tornadoes",
+  ];
+  const file_number = randomChoice(thousand);
+  const extensions = ["txt", "csv", "json", "xml"];
+  const ext = randomChoice(extensions);
+  const relative_path = `/sda/${randomChoice<number>(thousand)}/${randomChoice<string>(topics)}/file_${file_number}.${ext}`;
+
+  // const statuses = [
+  //   "created",
+  //   "available",
+  //   "unavailable",
+  //   "inconsistent_dataset",
+  // ];
+
+  const startDate_c = new Date(2024, 0, 1);
+  const endDate_c = new Date(2024, 1, 1);
+  const created_at = getRandomDate(startDate_c, endDate_c).toISOString();
+
+  // const startDate_u = new Date(2024, 1, 2);
+  // const endDate_u = new Date();
+  // const updated_at = getRandomDate(startDate_u, endDate_u).toISOString();
+
+  const datum = {
+    id: randomChoice(thousand).toString(),
+    name: `File names can be long ${file_number}`,
+    relativePath: relative_path,
+    createdAt: created_at,
+  };
+
+  return datum;
+};
+
+export const WithAlertSelectionButtons: Story = {
+  args: {
+    rowData: Array.from({ length: 106 }, dataGenerator),
+    isLoading: false,
+    download: {
+      isDownloading: false,
+      progress: 40,
+      onDownload: (selectedRows) => {
+        console.log(selectedRows);
+      },
+    },
+  },
+};
