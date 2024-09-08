52444024272a9ce3883981d3010cd2fbd9184ad9
Carla Bordo <carlapbg@gmail.com>
2024-09-08 22:34:31 +0200

    Site Header, stories and test

--- stories/layouts/SiteHeader.stories.tsx
+++ stories/layouts/SiteHeader.stories.tsx
@@ -66,6 +66,10 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  args: {},
+};
+
+export const WithClickActions: Story = {
   args: {
     children: menuItems,
   },
