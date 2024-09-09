d82c59634c7022a2e463378c04bc7fc3435467aa
maany <imptodefeat@gmail.com>
2024-09-10 00:32:11 +0200

    clean up menu, export shadcn components

--- stories/layouts/SiteHeader.stories.tsx
+++ stories/layouts/SiteHeader.stories.tsx
@@ -1,11 +1,54 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Header } from "@/components//site-header/SiteHeader";
-import { action } from "@storybook/addon-actions";
+import { NavigationMenuItem, NavigationMenuLink } from "@/ui/header";
+
+const handleItemClick = (item: string) => {
+  alert(`You clicked on ${item}`);
+};
+
+const menuItems = [
+  <NavigationMenuItem>
+    <NavigationMenuLink
+      href="/"
+      className="relative after:absolute after:left-0 after:top-5 after:w-0 after:h-1 after:bg-neutral-700 dark:after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+      onClick={() => handleItemClick("Home")}
+    >
+      Home
+    </NavigationMenuLink>
+  </NavigationMenuItem>,
+  <NavigationMenuItem>
+    <NavigationMenuLink
+      href="/"
+      className="relative after:absolute after:left-0 after:top-5 after:w-0 after:h-1 after:bg-neutral-700 dark:after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+      onClick={() => handleItemClick("Source")}
+    >
+      Source
+    </NavigationMenuLink>
+  </NavigationMenuItem>,
+  <NavigationMenuItem>
+    <NavigationMenuLink
+      href="/"
+      className="relative after:absolute after:left-0 after:top-5 after:w-0 after:h-1 after:bg-neutral-700 dark:after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+      onClick={() => handleItemClick("Research Context")}
+    >
+      Research Context
+    </NavigationMenuLink>
+  </NavigationMenuItem>,
+  <NavigationMenuItem>
+    <NavigationMenuLink
+      href="/"
+      className="relative after:absolute after:left-0 after:top-5 after:w-0 after:h-1 after:bg-neutral-700 dark:after:bg-white after:transition-all after:duration-300 hover:after:w-full"
+      onClick={() => handleItemClick("Documentation")}
+    >
+      Documentation
+    </NavigationMenuLink>
+  </NavigationMenuItem>,
+];
 
 export const TestPage = () => {
   return (
     <div>
-      <Header></Header> Holi Boli. 4got 2 tell u
+      <Header>{menuItems}</Header>
     </div>
   );
 };
@@ -23,11 +66,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
-};
-
-export const WithClickActions: Story = {
   args: {
-    onItemClick: action("onItemClick"),
+    children: menuItems,
   },
 };
