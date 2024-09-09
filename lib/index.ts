e04733a302ca6148b932904ccca25943dc7079ff
Carla Bordo <carlapbg@gmail.com>
2024-09-10 00:56:11 +0200

    Toast Index

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,6 @@ import "./tailwind/theme.css";
 export * from "./components";
 export * from "./tailwind";
 export { NavigationMenuItem, NavigationMenuLink } from "@/ui/header";
+export { ToastAction, ToastProvider } from "@/components/ui/toast";
+export { useToast } from "@/components/ui/use-toast";
+export { Toaster } from "@/components/ui/toaster";
