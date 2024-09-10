798a81ef356278c15d983713b1387055774361fc
Carla Bordo <carlapbg@gmail.com>
2024-09-10 12:41:41 +0200

    Style CreateResearchContextCreating and CreateResearchContextSelectFiles

--- lib/components/dialog/CreateResearchContextCreating.tsx
+++ lib/components/dialog/CreateResearchContextCreating.tsx
@@ -1,28 +1,30 @@
 import { TCreateResearchContextViewModel } from "../models";
+import { cn } from "@/utils/utils";
+import { Loader, CheckCircle, XCircle } from "lucide-react";
 
 export const CreateResearchContextCreating = (
   props: TCreateResearchContextViewModel,
 ) => {
   const icon = () => {
     if (props.status === "request") {
-      return "spinner";
+      return <Loader className="animate-spin text-neutral-800 w-6 h-6" />;
     }
     if (props.status === "progress") {
-      return "spinner";
+      return <Loader className="animate-spin text-neutral-500 w-6 h-6" />;
     }
     if (props.status === "error") {
-      return "error";
+      return <XCircle className="text-accent-error w-6 h-6" />;
     }
     if (props.status === "success") {
-      return "check";
+      return <CheckCircle className="text-accent-success w-6 h-6" />;
     }
   };
   const title = () => {
     if (props.status === "request") {
-      return "Creating research context...";
+      return "Requesting research context creation...";
     }
     if (props.status === "progress") {
-      return "Creating research context...";
+      return "Processing research context creation...";
     }
     if (props.status === "error") {
       return "Error";
@@ -46,10 +48,32 @@ export const CreateResearchContextCreating = (
     }
   };
   return (
-    <div className="flex flex-col gap-2 p-4 items-center justify-between">
-      {icon()}
-      <h1>{title()}</h1>
-      <p>{message()}</p>
+    <div className="flex flex-col gap-4 p-6 items-center justify-center bg-white shadow-md rounded-lg">
+      <div>{icon()}</div>
+
+      <h1
+        className={cn(
+          "text-xl font-bold",
+          props.status === "error" && "text-accent-error",
+          props.status === "success" && "text-accent-success",
+          (props.status === "request" || props.status === "progress") &&
+            "text-neutral-600",
+          props.status !== "error" &&
+            props.status !== "success" &&
+            "text-neutral-800",
+        )}
+      >
+        {title()}
+      </h1>
+
+      <p
+        className={cn(
+          "text-sm text-center",
+          props.status === "error" ? "text-accent-error" : "text-neutral-700",
+        )}
+      >
+        {message()}
+      </p>
     </div>
   );
 };
--- lib/components/dialog/CreateResearchContextSelectFiles.tsx
+++ lib/components/dialog/CreateResearchContextSelectFiles.tsx
@@ -1,5 +1,6 @@
 import { SelectableSourceDataAGGrid } from "../table";
 import { SelectableSourceDataRow } from "../table/SelectableSourceDataAGGrid";
+import { cn } from "@/utils/utils";
 
 export interface CreateResearchContextSelectFilesViewProps {
   sourceDataList: SelectableSourceDataRow[];
@@ -16,7 +17,7 @@ export const CreateResearchContextSelectFilesView = (
   props: CreateResearchContextSelectFilesViewProps,
 ) => {
   return (
-    <div>
+    <div className="p-5 space-y-1">
       <div>
         <SelectableSourceDataAGGrid
           rowData={props.sourceDataList}
@@ -25,8 +26,30 @@ export const CreateResearchContextSelectFilesView = (
           errorOverlayProps={props.errorOverlayProps}
         />
       </div>
-      <button onClick={props.onNext}>Next</button>
-      <button onClick={props.onPrevious}>Prev</button>
+
+      <div className="flex justify-between">
+        <button
+          onClick={props.onPrevious}
+          className={cn(
+            "bg-neutral-200 hover:bg-neutral-400 text-black",
+            "font-semibold py-2 px-4 rounded-lg shadow-md",
+            "transition-colors duration-300",
+          )}
+        >
+          Prev
+        </button>
+
+        <button
+          onClick={props.onNext}
+          className={cn(
+            "bg-neutral-200 hover:bg-neutral-400 text-black",
+            "font-semibold py-2 px-4 rounded-lg shadow-md",
+            "transition-colors duration-300",
+          )}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
