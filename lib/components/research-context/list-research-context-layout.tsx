a35b89b69086ce6be4af98e26909d04467f03863
Carla Bordo <carlapbg@gmail.com>
2024-09-10 02:04:09 +0200

    Research context card and list research context

--- lib/components/research-context/list-research-context-layout.tsx
+++ lib/components/research-context/list-research-context-layout.tsx
@@ -1,5 +1,42 @@
+"use client";
+import React from "react";
+import {
+  ResearchContextCard,
+  ResearchContextCardProps,
+} from "./research-context-card";
+
+export interface ListResearchContextCardProps {
+  items: ResearchContextCardProps[];
+}
+
 export const ListResearchContextLayout = (props: {
   children: React.ReactNode;
 }) => {
-  return <div className="w-full h-full flex">{props.children}</div>;
+  return (
+    <div
+      className="
+        grid
+        gap-6
+        grid-cols-1       
+        sm:grid-cols-2    
+        lg:grid-cols-3    
+        w-full
+        p-4
+      "
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export const ListResearchContextCard = ({
+  items,
+}: ListResearchContextCardProps) => {
+  return (
+    <ListResearchContextLayout>
+      {items.map((item) => (
+        <ResearchContextCard key={item.id} {...item} />
+      ))}
+    </ListResearchContextLayout>
+  );
 };
