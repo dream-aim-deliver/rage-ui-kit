c9ffe5af6a319a7cc836a57c243084453b49dd61
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-09-10 05:30:12 +0200

    temp: injecting SelectableSourceDataAGGrid in the research context dialog

--- lib/components/dialog/CreateResearchContextSelectFiles.tsx
+++ lib/components/dialog/CreateResearchContextSelectFiles.tsx
@@ -1,33 +1,29 @@
-import { RemoteFile } from "../models";
+import { SelectableSourceDataAGGrid } from "../table";
+import { SelectableSourceDataRow } from "../table/SelectableSourceDataAGGrid";
 
 export interface CreateResearchContextSelectFilesViewProps {
-  files: RemoteFile[];
-  selectFile: (file: RemoteFile) => void;
+  sourceDataList: SelectableSourceDataRow[];
+  isLoading: boolean;
+  handleConfirmSelection: (selectedRows: SelectableSourceDataRow[]) => void;
   onNext: () => void;
   onPrevious: () => void;
+  errorOverlayProps?: {
+    errorStatus: boolean;
+    overlayText: string;
+  };
 }
 export const CreateResearchContextSelectFilesView = (
   props: CreateResearchContextSelectFilesViewProps,
 ) => {
   return (
     <div>
-      <p>Select files</p>
       <div>
-        {props.files.map((file) => (
-          <div key={file.id}>
-            <input
-              type="checkbox"
-              id={file.name}
-              name={file.name}
-              value={file.name}
-              onChange={(e) => {
-                e.preventDefault();
-                props.selectFile(file);
-              }}
-            />
-            <label htmlFor={file.name}>{file.name}</label>
-          </div>
-        ))}
+        <SelectableSourceDataAGGrid
+          rowData={props.sourceDataList}
+          isLoading={props.isLoading}
+          handleConfirmSelection={props.handleConfirmSelection} 
+          errorOverlayProps={props.errorOverlayProps}
+        />
       </div>
       <button onClick={props.onNext}>Next</button>
       <button onClick={props.onPrevious}>Prev</button>
