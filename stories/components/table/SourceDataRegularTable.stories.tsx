866b00de01222420d61558974d287009a83bb934
maany <imptodefeat@gmail.com>
2024-10-14 18:41:56 +0200

    theme: ag-grid, clean-up

--- stories/components/table/SourceDataRegularTable.stories.tsx
+++ stories/components/table/SourceDataRegularTable.stories.tsx
@@ -7,7 +7,7 @@ const meta: Meta = {
   title: "Components/AGGrid/SourceDataRegularTable",
   component: SourceDataRegularTable,
   parameters: {
-    layout: "centered",
+    layout: "fullscreen",
   },
   tags: ["autodocs"],
 } satisfies Meta<typeof SourceDataRegularTable>;
