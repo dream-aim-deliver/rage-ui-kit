f2ddd74ede92b641d8cc2b7ebb1f0380abaf3bcb
MytsV <mytsvictoria@gmail.com>
2024-11-18 19:21:34 +0200

    Implement slider throttling and better typing

--- lib/components/case-study/CaseStudyPage.tsx
+++ lib/components/case-study/CaseStudyPage.tsx
@@ -7,7 +7,7 @@ import {
   DisasterDataSchema,
   DisasterDataTable,
 } from "@/components/table/case-study/DisasterDataTable.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DateSlider } from "@/components/case-study/DateSlider.tsx";
 import { CaseStudyTable } from "@/components/table/case-study/CaseStudyTable.tsx";
 import { ChatPage, TMessage } from "@/lib/components";
@@ -16,7 +16,7 @@ const ClimateKeyframeSchema = z.object({
   timestamp: z.string(),
   image: z.object({
     url: z.string().url(),
-    description: z.string(), // TODO: display as a hint
+    description: z.string(),
   }),
   caseStudy: z.literal("climate"),
   data: z.array(ClimateDataSchema),
@@ -26,7 +26,7 @@ const DisasterKeyframeSchema = z.object({
   timestamp: z.string(),
   image: z.object({
     url: z.string().url(),
-    description: z.string(), // TODO: display as a hint
+    description: z.string(),
   }),
   caseStudy: z.literal("disaster"),
   data: z.array(DisasterDataSchema),
@@ -51,22 +51,35 @@ export const CaseStudyPage = ({
 
   const [selectedTimestampIndex, setSelectedTimestampIndex] =
     useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const onTimestampChange = (newValue: number[]) => {
+    setIsLoading(true);
     setSelectedTimestampIndex(newValue[0]);
   };
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   const currentFrame = keyframes[selectedTimestampIndex];
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const tablesForCaseStudies: Record<string, CaseStudyTable<any>> = {
+  const tablesForCaseStudies: Record<
+    TKeyframe["caseStudy"],
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    CaseStudyTable<any>
+  > = {
     climate: ClimateDataTable,
     disaster: DisasterDataTable,
   };
+
   const Table = tablesForCaseStudies[currentFrame.caseStudy];
 
-  // TODO: implement slider throttling
-
+  // TODO: display image description as a hint on hover
   return (
     <div className="flex flex-row grow space-x-4">
       <div className="flex flex-1 flex-col grow space-y-4">
@@ -75,7 +88,10 @@ export const CaseStudyPage = ({
           style={{ backgroundImage: `url(${currentFrame.image.url})` }}
         ></div>
         <div className="flex flex-1 grow">
-          <Table rowData={currentFrame.data} isLoading={false} />
+          <Table
+            rowData={isLoading ? [] : currentFrame.data}
+            isLoading={isLoading}
+          />
         </div>
         {timelineEnabled && (
           <DateSlider
