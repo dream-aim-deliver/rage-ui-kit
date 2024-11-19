7551cd21d16a552a4e465eae078c6df52b0c1eaf
MytsV <mytsvictoria@gmail.com>
2024-11-19 10:57:56 +0200

    Enhance throttling handling

--- lib/components/case-study/CaseStudyPage.tsx
+++ lib/components/case-study/CaseStudyPage.tsx
@@ -7,11 +7,12 @@ import {
   DisasterDataSchema,
   DisasterDataTable,
 } from "@/components/table/case-study/DisasterDataTable.tsx";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { DateSlider } from "@/components/case-study/DateSlider.tsx";
 import { CaseStudyTable } from "@/components/table/case-study/CaseStudyTable.tsx";
 import { ChatPage, TMessage } from "@/lib/components";
 import { Skeleton } from "@/ui/skeleton.tsx";
+import { clearInterval } from "node:timers";
 
 const BaseKeyframeSchema = z.object({
   timestamp: z.string(),
@@ -60,18 +61,24 @@ export const CaseStudyPage = ({
     useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const throttleIntervalId = useRef<any>(undefined);
+
   const onTimestampChange = (newValue: number[]) => {
     setIsLoading(true);
     setSelectedTimestampIndex(newValue[0]);
+
+    if (throttleIntervalId.current) {
+      throttleIntervalId.current = setInterval(() => {
+        setIsLoading(false);
+
+        return () => {
+          clearInterval(throttleIntervalId.current);
+          throttleIntervalId.current = undefined;
+        };
+      }, 1500);
+    }
   };
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIsLoading(false);
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
 
   const currentFrame = keyframes[selectedTimestampIndex];
 
